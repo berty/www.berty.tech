7959bfa84c9ba82926e7cb3865609b84c4ee6c54
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2020-04-20 19:36:02 +0700

    chore: Add amp-validator to post-build

--- bertytech/post-build.sh
+++ bertytech/post-build.sh
@@ -3,6 +3,7 @@ set -e
 
 node ./optimize.js
 node ./webp-converter.js
+node ./validate-amp.js
 
 if [ x${CONTEXT} == "xproduction" ]; then
     curl https://www.google.com/ping?sitemap=https://berty.tech/sitemap.xml
