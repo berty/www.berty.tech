fb239f4c59f698523f2b2763a32e8c23a77c81e7
levanton21 <levanton21@gmail.com>
2022-01-13 13:44:50 +0200

    faq jumping fix

--- bertytech/postcss.config.js
+++ bertytech/postcss.config.js
@@ -1,5 +1,5 @@
 const purgeCssWhitelist = {
-  whitelist: ['value-true','value-false','value-partially', 'highlight', 'anchor-heading', 'anchor-link', 'img', 'show', 'blockquote', 'pre', 'ol', 'h6', 'table', 'tbody', 'thead', 'tr', 'th', 'td', 'table-bordered', 'show', 'collapsing', 'section-id', 'block-id', 'block-id-avatar', 'block-id-name', 'block-id-qrcode', 'block-error', 'display-none', 'tag', 'blue', 'pink', 'yellow', 'black', 'bg-about', 'bg-download', 'bg-contact', 'toc', 'toc-list', 'toc-link', 'is-collapsible', 'is-collapsed', 'is-position-fixed', 'is-active-link', 'page-fake', 'bg-particles'],
+  whitelist: ['value-true','value-false','value-partially', 'highlight', 'anchor-heading', 'anchor-link', 'img', 'show', 'blockquote', 'pre', 'ol', 'h6', 'table', 'tbody', 'thead', 'tr', 'th', 'td', 'table-bordered', 'show', 'collapsing', 'section-id', 'block-id', 'block-id-avatar', 'block-id-name', 'block-id-qrcode', 'block-error', 'display-none', 'tag', 'blue', 'pink', 'yellow', 'black', 'bg-about', 'bg-download', 'bg-contact', 'toc', 'toc-list', 'toc-link', 'is-collapsible', 'is-collapsed', 'is-position-fixed', 'is-active-link', 'page-fake', 'bg-particles', 'faq-collapsing'],
   whitelistPatterns: [/^slick/],
 }
 
